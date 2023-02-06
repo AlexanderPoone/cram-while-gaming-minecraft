85eb6ecd09ab11db43ac6c13f501e42ac0349504
SoftFeta <mundanesurrealism@gmail.com>
2023-02-06 14:22:03 +0800

    .'

---
+++ generate.py
@@ -0,0 +1,29 @@
+from json import *
+from os.path import expanduser
+from glob import glob
+from zipfile import ZipFile
+
+lang = 'es_es'
+
+hash = loads(open(expanduser('~/AppData/Roaming/.minecraft/assets/indexes/2.json'), 'r', encoding='utf-8').read())['objects'][f'minecraft/lang/{lang}.json']['hash']
+# hash = loads(open(expanduser('~/AppData/Roaming/.minecraft/assets/indexes/1.19.json'), 'r', encoding='utf-8').read())['objects']['minecraft/lang/en_gb.json']['hash']
+print(hash)
+
+trans = loads(open(expanduser(f'~/AppData/Roaming/.minecraft/assets/objects/{hash[:2]}/{hash}'), 'r', encoding='utf-8').read())
+out = loads(ZipFile(expanduser('~/AppData/Roaming/.minecraft/versions/1.19.3/1.19.3.jar')).open('assets/minecraft/lang/en_us.json').read())
+
+for k in trans:
+	if k in out:
+		out[k] = trans[k] + ' / ' + out[k]
+
+for mod in ('comforts', 'biomesoplenty',):
+	mod_trans = loads(ZipFile(glob(expanduser(f'~/AppData/Roaming/.minecraft/mods/{mod}*.jar'))[0]).open(f'assets/{mod}/lang/{lang}.json').read().decode('utf-8'))
+	mod_en = loads(ZipFile(glob(expanduser(f'~/AppData/Roaming/.minecraft/mods/{mod}*.jar'))[0]).open(f'assets/{mod}/lang/en_us.json').read().decode('utf-8'))
+
+	for k in mod_trans:
+		if k in mod_en:
+			mod_en[k] = mod_trans[k] + ' / ' + mod_en[k]
+	for k in mod_en:
+		out[k] = mod_en[k]
+
+open(f'cm_{lang[:2]}.json', 'w', encoding='utf-8').write(dumps(out, indent=2))
