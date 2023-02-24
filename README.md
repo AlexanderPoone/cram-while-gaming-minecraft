398f68b855b61793540693e1e69956a67b46ef30
SoftFeta <mundanesurrealism@gmail.com>
2023-02-24 09:53:07 +0800

    .

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # unofficial-mc-mods-translations
-Yet to be updated.
+This is a personal repo for cramming words in foreign languages while playing Minecraft, hence the term *crammese*. I don't have much time and my life is dreadful.
+
+Requirements:
+* Minecraft 1.19.3
--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ from os.path import expanduser
 from glob import glob
 from zipfile import ZipFile
 
-lang = 'es_es'
+lang = 'fr_fr'
 
 hash = loads(open(expanduser('~/AppData/Roaming/.minecraft/assets/indexes/2.json'), 'r', encoding='utf-8').read())['objects'][f'minecraft/lang/{lang}.json']['hash']
 # hash = loads(open(expanduser('~/AppData/Roaming/.minecraft/assets/indexes/1.19.json'), 'r', encoding='utf-8').read())['objects']['minecraft/lang/en_gb.json']['hash']
@@ -16,17 +16,17 @@ for k in trans:
 	if k in out:
 		out[k] = trans[k] + ' / ' + out[k]
 
-for mod in ('comforts', 'biomesoplenty','cfm','jei'):
+for mod in ('comforts', 'biomesoplenty','cfm','jei','sereneseasons','mcw-paths','mcw-roofs','mcw-bridges','mcw-trapdoors','mcw-doors','mcw-windows','mcw-fences','mcw-lights','mcw-paintings','wthit','xaeros_minimap','moonlight'):
 	try:
-		mod_trans = loads(ZipFile(glob(expanduser(f'~/AppData/Roaming/.minecraft/mods/{mod}*.jar'))[0]).open(f'assets/{mod}/lang/{lang}.json').read().decode('utf-8'))
-		mod_en = loads(ZipFile(glob(expanduser(f'~/AppData/Roaming/.minecraft/mods/{mod}*.jar'))[0]).open(f'assets/{mod}/lang/en_us.json').read().decode('utf-8'))
+		mod_trans = loads(ZipFile(glob(expanduser(f'~/AppData/Roaming/.minecraft/mods/{mod}*.jar'))[0]).open(f'assets/{mod.replace("-","").replace("trap","trp").replace("wthit","waila").replace("xaeros_","xaero")}/lang/{lang}.json').read().decode('utf-8'))
+		mod_en = loads(ZipFile(glob(expanduser(f'~/AppData/Roaming/.minecraft/mods/{mod}*.jar'))[0]).open(f'assets/{mod.replace("-","").replace("trap","trp").replace("wthit","waila").replace("xaeros_","xaero")}/lang/en_us.json').read().decode('utf-8'))
 
 		for k in mod_trans:
 			if k in mod_en:
 				mod_en[k] = mod_trans[k] + ' / ' + mod_en[k]
 		for k in mod_en:
 			out[k] = mod_en[k]
-	except:
-		pass
+	except Exception as e:
+		print(e)
 
 open(f'cm_{lang[:2]}.json', 'w', encoding='utf-8').write(dumps(out, indent=2))
