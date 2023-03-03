91cc2e03beaf125a185189968606b8e860861d75
SoftFeta <mundanesurrealism@gmail.com>
2023-03-04 04:10:00 +0800

    .

--- generate.py
+++ generate.py
@@ -3,7 +3,7 @@ from os.path import expanduser
 from glob import glob
 from zipfile import ZipFile
 
-lang = 'fr_fr'
+lang = 'es_es'
 
 hash = loads(open(expanduser('~/AppData/Roaming/.minecraft/assets/indexes/2.json'), 'r', encoding='utf-8').read())['objects'][f'minecraft/lang/{lang}.json']['hash']
 # hash = loads(open(expanduser('~/AppData/Roaming/.minecraft/assets/indexes/1.19.json'), 'r', encoding='utf-8').read())['objects']['minecraft/lang/en_gb.json']['hash']
@@ -16,10 +16,10 @@ for k in trans:
 	if k in out:
 		out[k] = trans[k] + ' / ' + out[k]
 
-for mod in ('comforts', 'biomesoplenty','cfm','jei','sereneseasons','mcw-paths','mcw-roofs','mcw-bridges','mcw-trapdoors','mcw-doors','mcw-windows','mcw-fences','mcw-lights','mcw-paintings','wthit','xaeros_minimap','moonlight'):
+for mod in ('comforts', 'biomesoplenty','cfm','jei','sereneseasons','mcw-paths','mcw-roofs','mcw-bridges','mcw-trapdoors','mcw-doors','mcw-windows','mcw-fences','mcw-lights','mcw-paintings','wthit','xaeros_minimap','moonlight','betterpvp'):
 	try:
-		mod_trans = loads(ZipFile(glob(expanduser(f'~/AppData/Roaming/.minecraft/mods/{mod}*.jar'))[0]).open(f'assets/{mod.replace("-","").replace("trap","trp").replace("wthit","waila").replace("xaeros_","xaero")}/lang/{lang}.json').read().decode('utf-8'))
-		mod_en = loads(ZipFile(glob(expanduser(f'~/AppData/Roaming/.minecraft/mods/{mod}*.jar'))[0]).open(f'assets/{mod.replace("-","").replace("trap","trp").replace("wthit","waila").replace("xaeros_","xaero")}/lang/en_us.json').read().decode('utf-8'))
+		mod_trans = loads(ZipFile(glob(expanduser(f'~/AppData/Roaming/.minecraft/mods/{mod}*.jar'))[0]).open(f'assets/{mod.replace("-","").replace("trap","trp").replace("wthit","waila").replace("xaeros_","xaero").replace("betterpvp","xaerobetterpvp")}/lang/{lang}.json').read().decode('utf-8'))
+		mod_en = loads(ZipFile(glob(expanduser(f'~/AppData/Roaming/.minecraft/mods/{mod}*.jar'))[0]).open(f'assets/{mod.replace("-","").replace("trap","trp").replace("wthit","waila").replace("xaeros_","xaero").replace("betterpvp","xaerobetterpvp")}/lang/en_us.json').read().decode('utf-8'))
 
 		for k in mod_trans:
 			if k in mod_en:
