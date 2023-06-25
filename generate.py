01f457fa5dfad747eda8beba8d87771b0fe4a1ac
SoftFeta <mundanesurrealism@gmail.com>
2023-06-25 14:25:45 +0800

    '.'

--- generate.py
+++ generate.py
@@ -6,23 +6,23 @@ from re import sub
 
 lang = 'ja_jp'
 
-hash = loads(open(expanduser('~/AppData/Roaming/.minecraft/assets/indexes/4.json'), 'r', encoding='utf-8').read())['objects'][f'minecraft/lang/{lang}.json']['hash']
+hash = loads(open(expanduser('~/AppData/Roaming/.minecraft/assets/indexes/5.json'), 'r', encoding='utf-8').read())['objects'][f'minecraft/lang/{lang}.json']['hash']
 # hash = loads(open(expanduser('~/AppData/Roaming/.minecraft/assets/indexes/1.19.json'), 'r', encoding='utf-8').read())['objects']['minecraft/lang/en_gb.json']['hash']
 print(hash)
 
 trans = loads(open(expanduser(f'~/AppData/Roaming/.minecraft/assets/objects/{hash[:2]}/{hash}'), 'r', encoding='utf-8').read())
-out = loads(ZipFile(expanduser('~/AppData/Roaming/.minecraft/versions/23w14a/23w14a.jar')).open('assets/minecraft/lang/en_us.json').read())
+out = loads(ZipFile(expanduser('~/AppData/Roaming/.minecraft/versions/1.20/1.20.jar')).open('assets/minecraft/lang/en_us.json').read())
 
 for k in trans:
 	if k in out:
 		if lang.startswith('es_'):
-			out[k] = sub('(?<!C|S|c|s)h', '<h>', trans[k].replace('iencio','`iencio').replace('iencia','`iencia').replace('ienso','`ienso').replace('iensa','`iensa').replace('iense','`iense').replace('iento','`iento').replace('ienta','`ienta').replace('iente','`iente').replace('iende','`iende').replace('iando','`iando').replace('ianda','`ianda').replace('uevo','`uevo').replace('ueva','`ueva').replace('ueve','`ueve').replace('ier','`ier').replace('uer','`uer').replace('v','v(b)').replace('H','<H>').replace('V','V(B)')) + ' / ' + out[k].replace('%s','')
+			out[k] = sub('(?<!C|S|P|c|s|p)h', '<h>', trans[k].replace('iencio','`iencio').replace('iencia','`iencia').replace('ienso','`ienso').replace('iensa','`iensa').replace('iense','`iense').replace('iento','`iento').replace('ienta','`ienta').replace('iente','`iente').replace('iende','`iende').replace('iando','`iando').replace('ianda','`ianda').replace('uevo','`uevo').replace('ueva','`ueva').replace('ueve','`ueve').replace('ier','`ier').replace('uer','`uer').replace('v','v(b)').replace('H','<H>').replace('V','V(B)')) + ' / ' + out[k].replace('%s','')
 		elif lang.startswith('fr_'):
-			out[k] = sub('(?<!C|S|c|s)h', '<h>', trans[k]) + ' / ' + out[k].replace('%s','')
+			out[k] = sub('(?<!C|S|P|c|s|p)h', '<h>', trans[k]) + ' / ' + out[k].replace('%s','')
 		else:
 			out[k] = trans[k] + ' / ' + out[k].replace('%s','')
 
-for mod in ('comforts', 'biomesoplenty','cfm','jei','sereneseasons','mcw-paths','mcw-roofs','mcw-bridges','mcw-trapdoors','mcw-doors','mcw-windows','mcw-fences','mcw-lights','mcw-paintings','wthit','xaeros_minimap','moonlight','betterpvp','jade'):
+for mod in ('comforts', 'biomesoplenty','cfm','jei','sereneseasons','mcw-paths','mcw-roofs','mcw-bridges','mcw-trapdoors','mcw-doors','mcw-windows','mcw-fences','mcw-lights','mcw-paintings','wthit','xaeros_minimap','moonlight','betterpvp','jade', 'advancementframes', 'betterlily', 'dummmmmmy', 'goated', 'hauntedharvest', 'heartstone', 'jeed', 'labels', 'moyai', 'sleep_tight', 'smarterfarmers', 'snowyspirit', 'supplementaries', 'suppsquared'):
 	try:
 		mod_trans = loads(ZipFile(glob(expanduser(f'~/AppData/Roaming/.minecraft/mods/{mod}*.jar'))[0]).open(f'assets/{mod.replace("-","").replace("trap","trp").replace("wthit","waila").replace("xaeros_","xaero").replace("betterpvp","xaerobetterpvp")}/lang/{lang}.json').read().decode('utf-8'))
 		mod_en = loads(ZipFile(glob(expanduser(f'~/AppData/Roaming/.minecraft/mods/{mod}*.jar'))[0]).open(f'assets/{mod.replace("-","").replace("trap","trp").replace("wthit","waila").replace("xaeros_","xaero").replace("betterpvp","xaerobetterpvp")}/lang/en_us.json').read().decode('utf-8'))
@@ -30,9 +30,9 @@ for mod in ('comforts', 'biomesoplenty','cfm','jei','sereneseasons','mcw-paths',
 		for k in mod_trans:
 			if k in mod_en:
 				if lang.startswith('es_'):
-					mod_en[k] = sub('(?<!C|S|c|s)h', '<h>', mod_trans[k].replace('iencio','`iencio').replace('iencia','`iencia').replace('ienso','`ienso').replace('iensa','`iensa').replace('iense','`iense').replace('iento','`iento').replace('ienta','`ienta').replace('iente','`iente').replace('iende','`iende').replace('iando','`iando').replace('ianda','`ianda').replace('uevo','`uevo').replace('ueva','`ueva').replace('ueve','`ueve').replace('ier','`ier').replace('uer','`uer').replace('v','v(b)').replace('H','<H>').replace('V','V(B)')) + ' / ' + mod_en[k].replace('%s','')
+					mod_en[k] = sub('(?<!C|S|P|c|s|p)h', '<h>', mod_trans[k].replace('iencio','`iencio').replace('iencia','`iencia').replace('ienso','`ienso').replace('iensa','`iensa').replace('iense','`iense').replace('iento','`iento').replace('ienta','`ienta').replace('iente','`iente').replace('iende','`iende').replace('iando','`iando').replace('ianda','`ianda').replace('uevo','`uevo').replace('ueva','`ueva').replace('ueve','`ueve').replace('ier','`ier').replace('uer','`uer').replace('v','v(b)').replace('H','<H>').replace('V','V(B)')) + ' / ' + mod_en[k].replace('%s','')
 				elif lang.startswith('fr_'):
-					out[k] = sub('(?<!C|S|c|s)h', '<h>', mod_trans[k]) + ' / ' + mod_en[k].replace('%s','')
+					out[k] = sub('(?<!C|S|P|c|s|p)h', '<h>', mod_trans[k]) + ' / ' + mod_en[k].replace('%s','')
 				else:
 					mod_en[k] = mod_trans[k] + ' / ' + mod_en[k].replace('%s','')
 		for k in mod_en:
@@ -65,8 +65,8 @@ for mod in ('comforts', 'biomesoplenty','cfm','jei','sereneseasons','mcw-paths',
 					for k in mod_en:
 						out[k] = mod_en[k]
 				except Exception as ee:
-					print(64, ee)		
+					print(mod, 64, ee)		
 		else:
-			print(66, e)
+			print(mod, 66, e,)
 
 open(f'cm_{"ar" if lang == "es_ar" else lang[:2]}.json', 'w', encoding='utf-8').write(dumps(out, indent=2))
